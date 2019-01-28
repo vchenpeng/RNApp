a760dad9ee2b3ec2acab74cae272fa4cd1c719af
vchenpeng <vitoc@qq.com>
2019-01-28 20:41:58 +0800

    本地ajax提交

--- src/pages/Mine.js
+++ src/pages/Mine.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { Text, Image, View, StyleSheet, FlatList, StatusBar, AlertIOS, DatePickerIOS, TouchableOpacity } from 'react-native';
-import { Header, List, ListItem, Avatar, CheckBox } from 'react-native-elements';
+import { Text, Image, View, ScrollView, RefreshControl, Dimensions, Clipboard, NativeModules, StyleSheet, FlatList, StatusBar, AlertIOS, DatePickerIOS, TouchableOpacity } from 'react-native';
+import { Header, Button, ListItem, Avatar, CheckBox } from 'react-native-elements';
 import Ionicon from 'react-native-vector-icons/Ionicons';
+import { WebView } from "react-native-webview";
+import { Constants, Images, Colors } from "../resource";
+import DropDownHolder from '../utils/DropDownHolder';
 
+let cookie = '';
 export default class Mine extends Component {
+    webview: WebView
+
     //接收上一个页面传过来的title显示出来
     static navigationOptions = ({ navigation }) => ({
         headerTitle: '我的',
@@ -15,15 +21,12 @@ export default class Mine extends Component {
         },
         headerMode: "screen",
         headerStyle: { backgroundColor: '#376fff' },
-        //headerLeft: (<icon name="menu"></icon>),
         headerRight: (<View>
             <TouchableOpacity onPress={() => navigation.navigate('Web')} >
                 <Text style={{ paddingRight: 14, color: '#fff', fontSize: 18 }}>图文</Text>
             </TouchableOpacity>
         </View>),
-        headerLeft: null,
-        // headerBackTitle: '返回',
-        // headerTruncatedBackTitle: '返回'
+        headerLeft: null
     })
     constructor(props) {
         super(props);
@@ -31,30 +34,51 @@ export default class Mine extends Component {
             text: '',
             user: null,
             chosenDate: new Date(),
-            language: '.net'
-        };
-        this.setDate = this.setDate.bind(this);
-        this.goScan = this.goScan.bind(this);
-    }
+            language: '.net',
+            refreshing: false,
 
-    fetchUserList() {
-        const url = 'https://api.jinse.com/v4/coin/change/ranks?order=up&limit=50';
-        fetch(url)
+            cookie: null,
+            id: null,
+            pid: 91286199,
+            products: []
+        };
+    }
+    searchWPH(title) {
+        let that = this;
+        const url = `https://m.vip.com/server.html?rpc&method=SearchRpc.getSearchList&f=www`;
+        return fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                "method": "SearchRpc.getSearchList",
+                "params": {
+                    "page": "searchlist.html",
+                    "channel_id": "",
+                    "keyword": title,
+                    "np": 1,
+                    "ep": 20,
+                    "brand_ids": "",
+                    "brand_store_sn": "",
+                    "props": "",
+                    "sort": 0,
+                    "category_id_1_show": "",
+                    "category_id_1_5_show": "",
+                    "category_id_2_show": "",
+                    "category_id_3_show": "",
+                    "query": ""
+                },
+                "id": 1548416963527,
+                "jsonrpc": "2.0"
+            }),
+            headers: {
+                "authorization": "OAuth api_sign=9499c033242a2dd1a0e38e03a136acd86d17d194",
+            },
+            credentials: 'include'
+        })
             .then((response) => response.json())
-            .then((responseJson) => {
-                var list = responseJson;
-                this.setState({
-                    list: list
-                });
-            })
-            .catch((error) => console.error(error))
+            .catch((error) => console.error(error));
     }
     componentDidMount() {
-        this.fetchUserList();
-    }
-
-    setDate(newDate) {
-        this.setState({ chosenDate: newDate });
+        NativeModules.MainBridge.setIdleTimerDisabled(true);
     }
     renderRow({ item }) {
         return (
@@ -75,29 +99,110 @@ export default class Mine extends Component {
             </TouchableOpacity>
         )
     }
-    goScan() {
-        const { navigate } = this.props.navigation;
-        navigate('Web');
-    }
-
     render() {
+        let that = this;
+        let { height, width } = Dimensions.get('window');
         const { navigate } = this.props.navigation;
-        return (<View style={{ flex: 1, flexDirection: 'column' }}>
+        let _scrollView = ScrollView;
+
+        return (<View style={{ flex: 1, backgroundColor: "#fff" }}>
             <StatusBar barStyle="light-content" />
-            {/* <TouchableOpacity style={{}}
-                onPress={() => navigate('Web')} >
-                <Header
-                    statusBarProps={{ barStyle: 'light-content' }}
-                    leftComponent={{ icon: 'home', color: '#fff', size: 22 }}
-                    backgroundColor="#376fff"
-                    centerComponent={{ text: '首页首页首页首页', style: { color: '#fff', fontSize: 18 } }}
-                    rightComponent={{ icon: 'menu', color: '#fff', size: 22 }}
-                />
-            </TouchableOpacity> */}
-            <FlatList
-                data={this.state.list}
-                renderItem={this.renderRow}
-                keyExtractor={(item, index) => index.toString()}
+            <WebView
+                ref={w => this.webview = w}
+                source={{ uri: "https://m.beidian.com/login/fast_login.html" }}
+                startInLoadingState={true}
+                hideKeyboardAccessoryView={true}
+                allowsBackForwardNavigationGestures={true}
+                allowsLinkPreview={true}
+                decelerationRate="normal"
+                dataDetectorTypes="none"
+                scrollEnabled={false}
+                renderLoading={() => (<View
+                    style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#fff" }}
+
+                >
+                    {/* <Image source={Images.ic_webloading} /> */}
+                </View>)}
+                injectedJavaScript={`
+                    var pid = 91286199;
+                    function ajax() {
+                        $.ajax({
+                            type: "GET",
+                            url: "https://imapi.beidian.com/server/gateway?method=voc.price.hunter.mission.change&uid=91286199&pid=" + pid,
+                            data: null,
+                            xhrFields: {
+                                withCredentials: true
+                            },
+                            success: (data) => {
+                                if(data.success){
+                                    pid = data.body.productInfo.pid;
+                                    //alert(data.body.productInfo.title,"提示");
+                                    window.postMessage(JSON.stringify(data.body.productInfo));
+                                }
+                            },
+                            error: () => {
+    
+                            }
+                        });
+                    }
+                    function submitBD(id,url){
+                        $.ajax({
+                            type: "POST",
+                            url: "https://imapi.beidian.com/server/gateway?method=voc.pricetask.submitMission",
+                            contentType: 'application/json;charset=utf-8',
+                            dataType: 'json',
+                            data: JSON.stringify({
+                                "uid": 91286199,
+                                "outerUrl": url,
+                                "id": id
+                            }),
+                            xhrFields: {
+                                withCredentials: true
+                            },
+                            success: (data) => {
+                                //alert(JSON.stringify(data));
+                            },
+                            error: (error) => {
+                                //alert(JSON.stringify(error));
+                            }
+                        });
+                    }
+                    setInterval(()=>{
+                        ajax();
+                    },3000);
+                    
+                    setTimeout(()=>{
+                        document.addEventListener("message", function(event){
+                            var data = JSON.parse(event.data);
+                            submitBD(data.id,data.url);
+                        }, false);
+                    },3000);
+                `}
+                userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+                onMessage={async (event) => {
+                    var data = JSON.parse(event.nativeEvent.data);
+                    let products = this.state.products;
+                    products.push(data);
+                    this.setState({
+                        products: products
+                    });
+                    var wphInfo = await that.searchWPH(data.title);
+                    DropDownHolder.alert(data.title, '', 'info');
+                    var wphInfoData = wphInfo[0]["result"]["data"];
+                    if (wphInfoData) {
+                        let url = "https://m.vip.com"
+                            + wphInfoData["products"][0]["product_url"]
+                            + "?msns=iphone-6.36-link&st=p-url&cid=6225bca09bcdf0cc9da18b7a0f9072261118a72d&chl_param=share%3A6d5cvgvrP8&abtid=13&uid=";
+
+                        Clipboard.setString(url);
+                        let postStr = JSON.stringify({
+                            id: data.id,
+                            uid: 91286199,
+                            url: url
+                        });
+                        this.webview.postMessage(postStr);
+                    }
+                }}
             />
         </View>)
     }
